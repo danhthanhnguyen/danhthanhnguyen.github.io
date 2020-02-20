641b873be05ec43f5c6ab78cbd7ee6fe6e8d7e9e
Nguyen Danh Thanh <54132194+danhthanhnguyen@users.noreply.github.com>
2020-02-20 23:47:08 +0700

    Update footer.js

--- src/component/footer/footer.js
+++ src/component/footer/footer.js
@@ -4,15 +4,14 @@ class Footer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            list: [0, 1],
             icon: ["tim108 icon ion-md-mail", "tim108 icon ion-md-call"],
             heading: ["Send me an email!", "Give me a ring"],
             content: ["nguyendanhthanh980@gmail.com", "+(84)-978605073"]
         }
     }
     render() {
-        const contactMe = this.state.list.map((items) => 
-        <div key={items.toString()} className="tim105"><div className="tim106"><div className="tim107"><i className={this.state.icon[items]}></i></div><div className="tim109"><h4 className="tim110">{this.state.heading[items]}</h4></div></div><div className="tim111"><span className="tim112">{this.state.content[items]}</span></div></div>
+        const contactMe = this.state.icon.map((value, index) => 
+        <div key={index.toString()} className="tim105"><div className="tim106"><div className="tim107"><i className={value}></i></div><div className="tim109"><h4 className="tim110">{this.state.heading[index]}</h4></div></div><div className="tim111"><span className="tim112">{this.state.content[index]}</span></div></div>
         );
         return (
             <div className="tim98">
